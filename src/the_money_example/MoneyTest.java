5c876ea14aa86d880b1d9092924c135f68e3e35d
0xNaN <enzodicicco@gmail.com>
2015-10-20 14:13:31 +0200

    introducing rates

--- src/the_money_example/MoneyTest.java
+++ src/the_money_example/MoneyTest.java
@@ -52,5 +52,10 @@ public class MoneyTest {
 		assertEquals(Money.dollar(1), result);
 	}
 
-
+	@Test
+	public void testReduceMoneyDifferentCurrency() {
+		Bank bank = new Bank();
+		bank.addRate("CHF", "USD", 2);
+		assertEquals(Money.dollar(1), bank.reduce(Money.franc(2), "USD"));
+	}
 }
