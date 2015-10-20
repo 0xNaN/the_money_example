5e0b0b3a7b0aa0de3850400a161e5b822f757f85
0xNaN <enzodicicco@gmail.com>
2015-10-20 12:59:45 +0200

    testing reducing of Money rather than Sum

--- src/the_money_example/Bank.java
+++ src/the_money_example/Bank.java
@@ -2,7 +2,9 @@ package the_money_example;
 
 public class Bank {
 
-	public Money reduce(Expression source,String to) {
+	public Money reduce(Expression source, String to) {
+		if(source instanceof Money)
+			return (Money) source;
 		Sum sum = (Sum) source;
 		return sum.reduce(to);
 	}
