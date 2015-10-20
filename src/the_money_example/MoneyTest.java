cd28b337c45249e07104702e7ecf58a764fba27e
0xNaN <enzodicicco@gmail.com>
2015-10-20 11:04:08 +0200

    adding factory method Money.franc

--- src/the_money_example/MoneyTest.java
+++ src/the_money_example/MoneyTest.java
@@ -9,7 +9,7 @@ public class MoneyTest {
 	public void testEquality() {
 		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
 		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-		assertTrue(new Franc(5).equals(new Franc(5)));
-		assertFalse(new Franc(5).equals(new Franc(6)));
+		assertTrue(Money.franc(5).equals(Money.franc(5)));
+		assertFalse(Money.franc(5).equals(Money.franc(6)));
 	}
 }
